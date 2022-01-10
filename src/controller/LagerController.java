4e0356ce917c678c16075b7a32ea24b63937df71
stefaniavlg <30491986+stefaniavlg@users.noreply.github.com>
2022-01-10 18:14:15 +0200

    finished controller

--- src/controller/LagerController.java
+++ src/controller/LagerController.java
@@ -38,6 +38,93 @@ public class LagerController {
         return null;
     }
 
+    /**
+     * Adding a new Lageraktualisierung to the list
+     * @param date of the aktualisierung
+     */
+    public void addLageraktualisierung(LocalDate date){
+        lageraktualisierungList.add(new Lageraktualisierung(date,new ArrayList<>()));
+    }
 
+    /**
+     * Adding new produkt
+     * @param produktName
+     * @param preis
+     * @param sku
+     * @param anzahl
+     */
+    public void addProdukt(String produktName, int preis, int sku, int anzahl){
+        produktList.add(new Produkt(produktName,preis,sku,anzahl));
+
+        }
+
+
+    /**
+     * Remove a specific Produkt if exist in list
+     * @param
+     * @return true if produkt has been removed and false if not in list
+     */
+    public boolean removeProdukt(String name){
+        Produkt produkt = findProdukt(name);
+        if(produkt==null)
+            return false;
+        produktList.remove(produkt);
+        return true;
+    }
+
+    /**
+     * Removes a specific lageraktualisierung
+     * @param
+     * @param
+     * @return true if lageraktualisierung has been removed and false if not in list
+     */
+    public boolean removeLageraktualisierung(LocalDate date,String name){
+        Lageraktualisierung lageraktualisierung = findLagerakutalisierung(date);
+        Produkt produkt = findProdukt(name);
+        if(produkt==null)
+            return false;
+        var newProduktList = lageraktualisierung.getProduktList();
+        newProduktList.remove(produkt);
+        lageraktualisierung.setProduktList(newProduktList);
+        return true;
+    }
+
+    /**
+     * Update Lageraktualisierung
+     * @param date
+     * @param updatedLager
+     * @return
+     */
+    public boolean updateLageraktualisierung(LocalDate date,Lageraktualisierung updatedLager){
+        Lageraktualisierung lageraktualisierung = findLagerakutalisierung(date);
+        if(lageraktualisierung==null)
+            return false;
+        lageraktualisierung.setDate(updatedLager.getDate());
+        lageraktualisierung.setProduktList(updatedLager.getProduktList());
+        return true;
+    }
+
+    /**
+     * Update produkt
+     * @param name
+     * @param preis
+     * @param sku
+     * @param anzahl
+     * @param updatedProdukt
+     * @return
+     */
+    public boolean updateProdukt(String name, int preis, int sku, int anzahl, Produkt updatedProdukt){
+        Produkt produkt = findProdukt(name);
+        if(produkt==null)
+            return false;
+        produkt.setName(updatedProdukt.getName());
+        produkt.setPreis(updatedProdukt.getPreis());
+        produkt.setSku(updatedProdukt.getSku());
+        produkt.setAnzahl(updatedProdukt.getAnzahl());
+
+        return true;
+    }
 
 }
+
+
